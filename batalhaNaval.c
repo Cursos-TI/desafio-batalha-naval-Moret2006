3e02b2406075ac602b4907df3375af2e0fa1c264
Moret <200856686+Moret2006@users.noreply.github.com>
2025-05-19 21:03:26 +0000

    teste

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
+int main (){
 
-int main() {
     char letra[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    char tabuleiro[10][10];
+    int tabuleiro[10][10];
 
-    // inicializar o tabuleiro com água (~)
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // posição dos navios 
-    tabuleiro[0][0] = 1; // A1
-    tabuleiro[1][0] = 1; 
-    tabuleiro[2][0] = 1; 
-    tabuleiro[3][9] = 1; 
-    tabuleiro[3][8] = 1; 
+     tabuleiro [0][0]= 1;
+     tabuleiro [0][1]= 1;
 
-    // espaço vazio entre "A" e "1"
-    printf("   ");
-    for (int i = 0; i < 10; i++) {
-        printf("%c  ", letra[i]);
+     tabuleiro [4][3]=1;
+     tabuleiro [5][3]=1;
+     
+    printf("  ");
+    for(int i = 0; i < 10; i ++)
+    {
+        printf("%c ", letra [i]);
     }
     printf("\n");
 
-    // imprimir tabuleiro com letras nas linhas
-    for (int i = 0; i < 10; i++) {
-        printf("%d ", i +1); 
-        for (int j = 0; j < 10; j++) {
-            printf(" %d ", tabuleiro[i][j]);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", i + 1);
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
         }
-        printf("\n");
+    printf("\n");
     }
+    
 
-    return 0;
 }
